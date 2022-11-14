1871aad89259c5af909d4305ad9d11010c8db017
miguelhuayhua <miguelhuayhuac2@gmail.com>
2022-11-14 12:00:27 -0400

    agregando heroku

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 from flask import Flask
-from routes.login import login
 from routes.usuarios import usuario
 from routes.mostrar import mostrar
-from routes.registro_user import usuarios
 from routes.session import login
 from pymysql import connect   #//por si falla la coneccion al DB
 from flask_cors import CORS
@@ -11,7 +9,6 @@ app = Flask(__name__)
 CORS(app)
 app.register_blueprint(login)
 app.register_blueprint(mostrar)
-app.register_blueprint(login)
 app.register_blueprint(usuario)
 
 
