4defb8ee3c7c62e86946c18618f96d1ae2ba9349
miguelhuayhua <miguelhuayhuac2@gmail.com>
2022-11-14 11:45:48 -0400

    agregando credenciales 14/11/22

--- routes/coneccion.py
+++ routes/coneccion.py
@@ -2,10 +2,11 @@ from pymysql import connect
 
 def db():
     db = connect(
-        host='localhost',
-        user='root', 
-        password = "",
-        db='pweb_python',
+        host='bvcpgguw0kpl5h91bxdy-mysql.services.clever-cloud.com',
+        user='u4plexpe8n2igv8k', 
+        password = "cgDOAUgJfonIDHUeJeqF",
+        db='bvcpgguw0kpl5h91bxdy',
         )
     return (db)
+
     
