e0df8bc7e9f01ba4810ed0e7fd4dcbd3b426e6f7
miguelhuayhua <105574278+miguelhuayhua@users.noreply.github.com>
2022-11-15 22:37:16 -0400

    Update coneccion.py

--- routes/coneccion.py
+++ routes/coneccion.py
@@ -1,16 +1,9 @@
 from pymysql import connect
-
 def db():
     db = connect(
-    try:
-        db = connect(
         host='bvcpgguw0kpl5h91bxdy-mysql.services.clever-cloud.com',
         user='u4plexpe8n2igv8k', 
         password = "cgDOAUgJfonIDHUeJeqF",
         db='bvcpgguw0kpl5h91bxdy',
         )
-    except(e):
-        print(e)
-    return (db)
-
-    
+    return db
